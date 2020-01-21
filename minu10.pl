b48d482771a67e55d8e991402a40ef568c997a78
minurex <59866037+minurex@users.noreply.github.com>
2020-01-21 16:07:29 +0530

    Add files via upload

---
+++ minu10.pl
@@ -0,0 +1,14 @@
+# To perform various array operation using perl
+#To perform various unshift operation using perl
+#!/user/bin/perl-w
+@bases=('A','C','G','T');
+$base2=shift@bases;
+push(@bases,$base1,$base2);
+print "here is the element from the end put on the begining:";
+print "@bases\n\n";
+print "...................";
+print"\n\n";
+exit;
+
+
+
