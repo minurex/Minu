0e03a7e66761398cb2c952af45b964107da182f9
minurex <59866037+minurex@users.noreply.github.com>
2020-01-21 15:40:37 +0530

    Add files via upload

---
+++ pkk.pl
@@ -0,0 +1,34 @@
+# To use conditioned statement if else
+#!/user/bin/perl-w
+#if-elsif-else
+$word='BME2020';
+if($word eq 'SEMESTERSIX')
+{
+print "SEMESTERSIX\n";
+}
+elsif($word eq 'BIOINFORMATICS')
+{
+print"BIOELECTRICITY\n";
+}
+elsif($word eq 'BME2020')
+{
+print "BME2020-MY DEPARTMENT!\n";
+}
+else
+{
+print "IS\"$word\"a technical institute? This program is not sure.\n";
+}
+print"...........................................";
+print"\n\n";
+exit;
+
+
+
+
+
+
+
+
+
+
+
