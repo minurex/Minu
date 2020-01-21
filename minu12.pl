d8a093c21cd0e0a5ef58a1261795557d21dbca60
minurex <59866037+minurex@users.noreply.github.com>
2020-01-21 16:22:54 +0530

    Add files via upload

---
+++ minu12.pl
@@ -0,0 +1,13 @@
+# To perform various array operation using perl
+#To perform various splice operation using perl
+#!/user/bin/perl
+@bases=('A','C','G','T');
+splice(@bases,2,0,'X');
+print "here is the array with an element inserted after the 2nd element:";
+print "@bases\n";
+print "...................";
+print"\n\n";
+exit;
+
+
+
