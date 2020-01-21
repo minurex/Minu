b61ca82e80f00e852daba250d5accc68b48e8844
minurex <59866037+minurex@users.noreply.github.com>
2020-01-21 16:11:33 +0530

    Add files via upload

---
+++ minu11.pl
@@ -0,0 +1,13 @@
+# To perform various array operation using perl
+#To perform various unshift operation using perl
+#!/user/bin/perl-w
+@bases=('A','C','G','T');
+@reverse=reverse@bases;
+print "here is the array in reverse:";
+print "@reverse\n\n";
+print "...................";
+print"\n\n";
+exit;
+
+
+
