bbf6f4615f36d7dcbe4e6c7dbab0e063ca80c9f5
minurex <59866037+minurex@users.noreply.github.com>
2020-01-21 15:51:10 +0530

    Add files via upload

---
+++ minu7.pl
@@ -0,0 +1,15 @@
+# To perform various array operation using perl
+#To perform various pob operation using perl
+#!/user/bin/perl-w
+@base=('A','C','G','T');
+$base1=pop@base;
+print"here is the element removed from the end:";
+print $base1,"\n";
+print "here is the remaining array of bases:";
+print @base,"\n";
+print"...................";
+print"\n\n";
+exit;
+
+
+
