a9975cd4996e39f4914e66ad18417cc4474294a6
minurex <59866037+minurex@users.noreply.github.com>
2020-01-21 15:56:54 +0530

    Add files via upload

---
+++ minu8.pl
@@ -0,0 +1,15 @@
+# To perform various array operation using perl
+#To perform various shift operation using perl
+#!/user/bin/perl-w
+@base=('A','C','G','T');
+$base1=shift@base;
+print"here is the element removed from the end:";
+print $base1,"\n";
+print "here is the remaining array of bases:";
+print @base,"\n";
+print"...................";
+print"\n\n";
+exit;
+
+
+
